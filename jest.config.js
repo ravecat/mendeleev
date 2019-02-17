e6952b2280502bb59ea243e031871843762e329c
Max Sharov <maxsharoff@gmail.com>
2019-02-17 23:04:07 +0300

    Update dev environment

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/*.{js,jsx,mjs}"],
-  setupFiles: ["<rootDir>/config/polyfills.js"],
-  testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}",
-    "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}"
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts"
   ],
-  testEnvironment: "node",
+  setupFiles: [
+    "react-app-polyfill/jsdom"
+  ],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
+  ],
+  testEnvironment: "jsdom",
   testURL: "http://localhost",
   moduleDirectories: ["node_modules"],
   transform: {
-    "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
     "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
   },
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"],
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
   moduleNameMapper: {
     "^react-native$": "react-native-web"
   },
