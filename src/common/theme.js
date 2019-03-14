b8845afe703a6519c936882ffa90b7417c695e1f
Max Sharov <maxsharoff@gmail.com>
2019-03-14 23:10:00 +0300

    Customize Element block

--- src/common/theme.js
+++ src/common/theme.js
@@ -9,10 +9,24 @@ export default {
     header: '16px',
     text: '14px',
     secondaryText: '12px',
+    thirdText: '10px',
     subText: '8px'
   },
   element: {
     width: 76,
-    height: 50
+    height: 50,
+    color: {
+      alkali: '#FEC590',
+      alkaline: '#FFDF92',
+      transition: '#ECFF91',
+      ['post-transition']: '#FEF991',
+      metalloids: '#91FF9E',
+      nonmetals: '#AB91FF',
+      halogens: '#F992FF',
+      noble: '#90DFFE',
+      unknown: '#C8C8C8',
+      lantanoids: '#D1FF92',
+      actinoids: '#B7FF91',
+    }
   }
 };
