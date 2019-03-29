f42eb0e7ac302c9ffe32d2d40beefa681ff9cc40
Max Sharov <maxsharoff@gmail.com>
2019-03-29 23:21:59 +0300

    Separate table views to different resolutions

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Responsive from 'react-responsive';
+<<<<<<< HEAD
 import { MICRO, STANDART } from 'common/config';
 import Element from './components/Element';
 
 function Table({ elements, maxPeriod }) {
+=======
+import { RESPONSIVE } from 'common/config';
+import Element from './components/Element';
+
+function Table({ elements, maxPeriod }) {
+  const [MICRO, STANDART] = RESPONSIVE;
+
+>>>>>>> Separate table views to different resolutions
   return(
     <>
       <Responsive minWidth={ STANDART + 1 } >
